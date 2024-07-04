196b946fe539a9e9a5fc9273aefc988134d37236
MADEYE <52187012+seyi91@users.noreply.github.com>
2024-07-03 19:27:56 -0500

    Refactor LandingPage, add HomePage with link from LandingPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react'
+import MainSlice from './components/LandingPage';
+import HomePage from './components/HomePage';
+import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    return (
+      <Router>
+        <Routes>
+          <Route path ='/' element={<MainSlice />} />
+          <Route path ='home' element={<HomePage />} />
+        </Routes>
+      </Router>
+     )
 }
 
 export default App;
